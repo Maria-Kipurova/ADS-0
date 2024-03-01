302fc4963f6f8432f6364b3a1edb4c5f578d5779
Почемучка <159887975+Maria-Kipurova@users.noreply.github.com>
2024-03-01 21:07:49 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,8 @@
 // Copyright 2021 NNTU-CS
 
-int gcd(int a, int b) {// поместить сюда текст реализации алгоритма (на С++)
-    while (a != 0 && b != 0){
-        if (a >= b){
-            a %= b;}
-        else {
-            b %= a;}
+int gcd(int a, int b) {
+    if (b == 0) {
+        return a;
     }
+    return gcd(b, a % b);
 }
